246267d443270fa61b1b5453ac798918f0c07054
muradpunisher <muradsm@code.edu.az>
2023-03-28 08:58:55 +0400

    task

---
+++ script task/script.js
@@ -0,0 +1,165 @@
+// let const var
+
+// let var  deyisen deyisen
+// const  sabit deyisen
+
+
+// let x = 10;
+
+// x = 30;
+
+
+// let groupName = 'P138';
+
+// groupName = 'P148';
+
+// console.log(groupName);
+
+// var groupName = 'P138';
+
+// groupName = 'P145';
+
+// console.log(groupName);
+
+// let a = 'let';  //local deyisen
+
+// var aa = 'var'; // global deyisen
+
+
+// console.log(this);
+
+
+// if(true){
+//     var x = 10;
+// }
+
+// console.log(x);
+
+// const groupName = 'p138';
+
+// groupName = 'p148';
+
+// console.log(groupName);
+
+// let $name = 'Kenan';
+// let _name = "Sems";
+
+// number string boolean undefined null array object
+
+
+// let x = -10;
+// let y = Infinity;
+
+// let c = NaN;
+
+// ''  ""   ``;
+
+
+// let groupName = "-P188";
+
+// console.log('Code Academy' +  groupName );
+
+// console.log(`Code Academy  ${groupName}`);
+
+
+// let x = null;
+
+// let y = 10;
+// let cvb = y+x;
+// console.log(cvb);
+
+// let x;
+
+
+// let x = true;
+
+
+// let x = '10';
+
+// let y = '5';
+
+// console.log( x-y);
+
+
+// let y = null;
+
+// console.log(typeof(y));
+
+
+// let x = 10;
+
+// let y = 5;
+
+
+// alert(x<y);
+
+
+// let answer = prompt('Player1 adini daxil edin?', 'Admin');
+
+// let answer = confirm('Oyun bitdi yeni oyun baslasin?')
+
+// console.log(answer);
+
+
+// let x = 6;
+
+// x++;
+
+// console.log(++x);
+
+// console.log(x++);
+
+// console.log(x);
+
+
+// &&
+// || 
+// !
+
+
+// let x = 6;
+
+// let y = 6;
+
+// alert(x===y);
+
+
+// let answer = prompt('js ne zaman yarandi?');
+// let correctAnswer = '1995';
+
+
+// let result = (answer == correctAnswer) ? 'Duzgun' : (answer>1990 && answer<1995) ? 'Yaxinlasdiniz' : 'Yanlis';
+
+// alert(result);
+
+
+// if(answer == correctAnswer){
+//     alert('Duzgun');
+// }else if(answer>1990 && answer<1995 ){
+//     alert('yaxinlasdiniz')
+// }
+// else {
+//     alert('Yanlis');
+// }
+
+
+// let result = confirm('Oyun baslasinmi?');
+
+// if(result){
+// location.reload();
+// }
+
+let x = -1;
+
+let y = 1;
+
+let answer = prompt('eded menfidir yoxsa musbet?');
+
+if (answer > 0) {
+    console.log('musbet');
+} else {
+    console.log('menfi');
+}
+
+
+
